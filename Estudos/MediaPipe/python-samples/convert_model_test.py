301a4e292901c0204fe03b816892abe650242943
Lahis Almeida <lahis.gomes.almeida@gmail.com>
2025-01-13 18:27:11 -0400

    feat: add a header and comments

--- Estudos/MediaPipe/python-samples/convert_model_test.py
+++ Estudos/MediaPipe/python-samples/convert_model_test.py
@@ -1,14 +1,29 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+r"""
+This script perform a similiraty inference between text phrases using Bert Model and MediaPipe module.
+
+Usage:
+    $ python .\embedding_text.py
+"""
+
 import os
 from huggingface_hub import hf_hub_download
 from mediapipe.tasks.python.genai import converter
 
+token_secret = ""
 
 def gemma_download(token):
-  REPO_ID = "google/gemma-2b-it"
-  FILENAMES = ["tokenizer.json", "tokenizer_config.json", "model-00001-of-00002.safetensors", "model-00002-of-00002.safetensors"]
-  os.environ['HF_TOKEN'] = token
-  with out:
-    for filename in FILENAMES:
-      hf_hub_download(repo_id=REPO_ID, filename=filename, local_dir="./gemma-2b-it")
+    REPO_ID = "google/gemma-2b-it"
+    FILENAMES = ["tokenizer.json", "tokenizer_config.json", "model-00001-of-00002.safetensors", "model-00002-of-00002.safetensors"]
+    os.environ['HF_TOKEN'] = token
 
-gemma_download("")
+    for filename in FILENAMES:
+        hf_hub_download(repo_id=REPO_ID, filename=filename, local_dir="./gemma-2b-it")
+
+
+if __name__ == '__main__':
+
+    # Uncomment to donwload. Remember to use your hugging face token access with gema auth ok
+    # gemma_download(token_secret)
