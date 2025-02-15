d126a2a51bdb74752b399c05c54afb7fbc5b0a11
DevTitans <devtitans@icomp.ufam.edu.br>
2025-02-15 12:38:18 -0400

    add jni e lib

--- devtitans/kraken/devtitans_kraken.mk
+++ devtitans/kraken/devtitans_kraken.mk
@@ -6,36 +6,39 @@ PRODUCT_NAME := devtitans_kraken
 PRODUCT_DEVICE := kraken
 PRODUCT_BRAND := KrakenBrand
 PRODUCT_MODEL := KrakenModel
+
 # Copia o arquivo devtitans.txt para o /system/etc da imagem do Android
 PRODUCT_COPY_FILES += \
     device/devtitans/kraken/devtitans.txt:system/etc/devtitans.txt \
     device/devtitans/kraken/kraken.rc:vendor/etc/init/kraken.rc \
-    device/devtitans/kraken/bootanimation.zip:product/media/bootanimation.zip
+    device/devtitans/kraken/wallpaper2.png:/product/media/wallpaper2.png \
+    device/devtitans/kraken/init2.rc:vendor/etc/init/init2.rc
+
+PRODUCT_PACKAGES += \
+    UniversalMediaPlayer \
+    nano \
+    hello_c \
+    hello_cpp \
+    hello_daemon_cpp \
+    HelloJava \
+    smartlamp_client \
+    libllm_inference_engine_jni \
+    sl
+
+BOARD_SEPOLICY_DIRS += device/devtitans/kraken/sepolicy
 
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.devtitans.name=Kraken
+    ro.devtitans.name=Kraken \
+    ro.config.wallpaper=/product/media/wallpaper2.png \
+    ro.devtitas.teste2=teste2
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.product.devtitans.version=1.0
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.devtitans.hardware=ModelB
-
-# Seta o diretório de overlays
-PRODUCT_PACKAGE_OVERLAYS = device/devtitans/kraken/overlay
-
-PRODUCT_PACKAGES += \
-    UniversalMediaPlayer \
-    hello_c \
-    nano \
-    sl \
-    hello_cpp \
-    hello_daemon_cpp \
-    HelloApp \
-    smartlamp_client \
-
-
-BOARD_SEPOLICY_DIRS += device/devtitans/kraken/sepolicy
+    ro.vendor.devtitans.hardware=ModelB \
+    vendor.devtitans.smartlamp.allow_simulated=0 \
+    vendor.devtitans.smartlamp.disable_smartlamp=true
 
 # Smartlamp AIDL Interface
 PRODUCT_PACKAGES += devtitans.smartlamp
@@ -54,3 +57,8 @@ PRODUCT_PACKAGES += SmartlampTestApp
 
 # Manager
 PRODUCT_PACKAGES += devtitans.smartlampmanager
+
+
+
+# Seta o diretório de overlays
+PRODUCT_PACKAGE_OVERLAYS = device/devtitans/kraken/overlay
